6ad4a0b6afa414d638400026bc28f61015558e7d
Zidane-Han <han.gao.zidane@gmail.com>
2018-10-15 00:43:08 +0100

    fill Activity fragment (with bugs)

--- app/src/main/java/com/example/hangao/context_demo/ActivityFragment.java
+++ app/src/main/java/com/example/hangao/context_demo/ActivityFragment.java
@@ -1,20 +1,88 @@
 package com.example.hangao.context_demo;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class ActivityFragment extends Fragment{
+import com.google.android.gms.location.ActivityRecognitionClient;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+
+public class ActivityFragment extends Fragment implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     public static final String DETECTED_ACTIVITY = ".DETECTED_ACTIVITY";
+
+    private Context mContext;
+    private ActivityRecognitionClient mActivityRecognitionClient;
+    private ActivitiesAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.frag_activity_main, container, false);
 
+        mContext = this;
+
+        //Retrieve the ListView where we’ll display our activity data//
+        ListView detectedActivitiesListView = (ListView) v.findViewById(R.id.activities_listview);
+
+        ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
+                PreferenceManager.getDefaultSharedPreferences(this).getString(
+                        DETECTED_ACTIVITY, ""));
+
+        //Bind the adapter to the ListView//
+        mAdapter = new ActivitiesAdapter(this, detectedActivities);
+        detectedActivitiesListView.setAdapter(mAdapter);
+        mActivityRecognitionClient = new ActivityRecognitionClient(this);
+
         return v;
+    }
+
+    public void requestUpdatesHandler(View view) {
+        //Set the activity detection interval. I’m using 3 seconds//
+        Task<Void> task = mActivityRecognitionClient.requestActivityUpdates(
+                3000,
+                getActivityDetectionPendingIntent());
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void result) {
+                updateDetectedActivitiesList();
+            }
+        });
+    }
+
+    //Get a PendingIntent//
+    private PendingIntent getActivityDetectionPendingIntent() {
+        //Send the activity data to our DetectedActivitiesIntentService class//
+        Intent intent = new Intent(this, ActivityIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    }
+
+    //Process the list of activities//
+    protected void updateDetectedActivitiesList() {
+        ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
+                PreferenceManager.getDefaultSharedPreferences(mContext)
+                        .getString(DETECTED_ACTIVITY, ""));
+
+        mAdapter.updateActivities(detectedActivities);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(DETECTED_ACTIVITY)) {
+            updateDetectedActivitiesList();
+        }
     }
 }
